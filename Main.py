719f86e69ae4598333be0be154ff1a44083bb8ed
Oxyfoxys <oxyfoxys@yandex.ru>
2022-11-06 01:10:27 +0300

    Update

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
-print('Hello world!')
-print('Hello world!')
-print('Spam and eggs...')
+print('save me')
+print('rise revolution')
+print("shattered pices")
+print("from all this pain in the world")
+print("i dont understand")
+
+
+a = 1
+
+while a < 5:
+   print('условие верно ' + str(a) )
+   a = a + 1
+else:
+   print('условие неверно ' +str(a) )
