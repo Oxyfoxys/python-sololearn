02131738dc2cf8214d9bcc9d6d619ae5333475dd
Oxyfoxys <oxyfoxys@yandex.ru>
2022-11-14 00:31:44 +0300

    Update

--- Main.py
+++ Main.py
@@ -12,3 +12,93 @@ while a < 5:
    a = a + 1
 else:
    print('условие неверно ' +str(a) )
+
+   print(2 + 2)
+   print(5 + 4 - 3)
+
+   print(2 * (3 + 4))
+   print(10 / 2)
+
+   print(3 / 4)
+   print(0.42)
+
+   print(8 / 2)
+
+   print(6 * 7.0)
+
+   print(4 + 1.65)
+
+   print(1 + 2 + 3 + 4.0 + 5)
+
+   print(2 ** 5)
+
+   print(9 ** (1 / 2))
+
+   print(10//4)
+
+   print(20 % 6)
+
+   print(1.25 % 0.5)
+
+   print(7 % (5 // 2))
+
+   print((3 ** 2) // 2)
+
+   print('Coding in Finance')
+
+   print("An investment in knowledge pays the best interest.")
+
+   print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+
+   print('One\nTwo\nThree')
+
+   print("""this
+   is a
+   multiline
+   text""")
+
+   print("Spam" + 'eggs')
+
+   print("2" + "2")
+
+print("spam" * 3)
+
+print(4 * '2')
+
+x = 7
+print(x)
+
+print(x + 3)
+
+print(x)
+
+x = 123.456
+print(x)
+
+x = "This is a string"
+print(x + "!")
+
+age = 42
+print("His age is " + str(age))
+
+x = '2'
+y = '4'
+z = int(x) + int(y)
+print(z)
+
+x = 2
+print(x)
+
+x += 3
+print(x)
+
+x = "spam"
+print(x)
+
+x += "eggs"
+print(x)
+
+num = int(input())
+print(num)
+
+print(num:=int(input()))
