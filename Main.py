f71478850da193b9f0d940083160b20e1c6b5bba
Oxyfoxys <oxyfoxys@yandex.ru>
2022-11-25 11:18:12 +0300

    Update

--- Main.py
+++ Main.py
@@ -192,3 +192,27 @@ print((False == False) or True)
 grade = 88
 if(grade >= 70 and grade <= 100):
     print("Passed!")
+
+    words = ["Hello", "world", "!"]
+    print(words[0])
+    print(words[1])
+    print(words[2])
+
+    empty_list = []
+    print(empty_list)
+
+    number = 3
+    things = ["string", 0, [1, 2, number], 4.56]
+    print(things[1])
+    print(things[2])
+    print(things[2][2])
+
+    m = [
+        [1, 2, 3],
+        [4, 5, 6]
+    ]
+
+    print(m[1][2])
+
+    str = "Hello world!"
+    print(str[6])
