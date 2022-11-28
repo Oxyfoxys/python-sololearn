73fb8cb7abb9cad8b745cd94543018ce56791f20
Oxyfoxys <oxyfoxys@yandex.ru>
2022-11-28 19:58:06 +0300

    Update

--- Main.py
+++ Main.py
@@ -305,3 +305,28 @@ while i<5:
   words = ["hello", "world", "spam", "eggs"]
   for word in words:
       print(word + "!")
+
+      str = "testing for loops"
+      count = 0
+
+      for x in str:
+          if (x == 't'):
+              count += 1
+
+      print(count)
+
+      list = [2, 3, 4, 5, 6, 7]
+
+      for x in list:
+          if (x % 2 == 1 and x > 4):
+              print(x)
+              break
+
+
+
+
+for i in range(5):
+    print("hello!")
+
+list = [1, 1, 2, 3, 5, 8, 13]
+print(list[list[4]])
