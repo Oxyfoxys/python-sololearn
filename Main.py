4fb90123df2e8ec8fc772eeb671c7fb5fccddc42
Oxyfoxys <oxyfoxys@yandex.ru>
2022-12-01 16:38:23 +0300

    Update

--- Main.py
+++ Main.py
@@ -373,3 +373,44 @@ def function(variable):
 
 function(7)
 print("variable")
+
+
+def max(x, y):
+    if x >= y:
+        return x
+    else:
+        return y
+
+
+print(max(4, 7))
+z = max(8, 5)
+print(z)
+
+
+def add_numbers(x, y):
+    total = x + y
+    return total
+    print("This won't be printed")
+
+
+print(add_numbers(4, 5))
+
+x = 365
+y = 7
+# this is a comment
+
+print(x % y)  # find the remainder
+
+
+# print (x // y)
+# another comment
+
+def shout(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print(word + "!")
+
+
+shout("spam")
